e00b55525f9593cedc0f778b5606b2236d8aacc3
David Khuu <davidkhuu.dk@gmail.com>
2021-05-17 16:41:53 -0600

    Add folder level to dynamic route of posts
    Posts will have categories they belong to. This commit adds that additional level to allow this behaviour.

--- lib/api.ts
+++ lib/api.ts
@@ -5,24 +5,36 @@ import matter from 'gray-matter'
 const postsDirectory = join(process.cwd(), '_posts')
 
 export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory)
+  const postDirs = fs.readdirSync(postsDirectory)
+
+  type Slugs = {
+    [key: string]: string[]
+  }
+
+  const postSlugs: Slugs = {}
+  postDirs.forEach((category) => {
+    postSlugs[category] = fs.readdirSync(join(postsDirectory, category))
+  })
+
+  return postSlugs
 }
 
-export function getPostBySlug(slug: string, fields: string[] = []) {
+type Items = {
+  [key: string]: string
+}
+
+export function getPostBySlug(category: string, slug: string, fields: string[] = []) {
   const realSlug = slug.replace(/\.md$/, '')
-  const fullPath = join(postsDirectory, `${realSlug}.md`)
+  const fullPath = join(postsDirectory, category, `${realSlug}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
   const { data, content } = matter(fileContents)
-
-  type Items = {
-    [key: string]: string
-  }
 
   const items: Items = {}
 
   // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
     if (field === 'slug') {
+      items.category = category
       items[field] = realSlug
     }
     if (field === 'content') {
@@ -39,9 +51,13 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
 
 export function getAllPosts(fields: string[] = []) {
   const slugs = getPostSlugs()
-  const posts = slugs
-    .map((slug) => getPostBySlug(slug, fields))
-    // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+  let posts: Items[] = []
+  for (const [category, postSlugs] of Object.entries(slugs)) {
+    posts = posts.concat(
+      postSlugs.map((slug => getPostBySlug(category, slug, fields)))
+    )
+  }
+  // sort posts by date in descending order
+  posts = posts.sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
   return posts
 }
